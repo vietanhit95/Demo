12eeeab54543534c4e2a495b7b1c7290fef24fac
VietAnhNguyen <vietanhit95@gmail.com>
2016-08-13 17:00:24 +0700

    Edit Controller

---
+++ Demo_MVC/Demo_MVC/Controllers/Test2Controller.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Demo_MVC.Controllers
+{
+    public class Test2Controller : Controller
+    {
+        //
+        // GET: /Test2/
+        public ActionResult Index()
+        {
+            return View();
+        }
+	}
+}
